513dccc09edbfb7f59da7ab47005aa5c96467157
Boggy <138927149+boggy539@users.noreply.github.com>
2023-11-23 18:44:45 +0000

    Bug fixes, code improvement

---
+++ src/main/java/com/boggy/ispawners/gui/PageUtil.java
@@ -0,0 +1,34 @@
+package com.boggy.ispawners.gui;
+
+import org.bukkit.inventory.ItemStack;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class PageUtil {
+
+    public static List<ItemStack> getPageItems(List<ItemStack> items, int page, int spaces) {
+        int upperBound = page * spaces;
+        int lowerBound = upperBound - spaces;
+
+        List<ItemStack> newItems = new ArrayList<>();
+        for (int i = lowerBound; i < upperBound; i++) {
+            try {
+                newItems.add(items.get(i));
+            } catch (IndexOutOfBoundsException ex) {
+                break;
+            }
+        }
+        return newItems;
+    }
+
+    public static boolean isPageValid(List<ItemStack> items, int page, int spaces) {
+        if (page <= 0) { return false; }
+
+        int upperBound = page * spaces;
+        int lowerBound = upperBound - spaces;
+
+        return items.size() > lowerBound;
+    }
+
+}
---
+++ src/main/java/com/boggy/ispawners/pdc/SpawnerUtils.java
@@ -0,0 +1,103 @@
+package com.boggy.ispawners.pdc;
+
+import com.boggy.ispawners.ISpawners;
+import net.md_5.bungee.api.ChatMessageType;
+import net.md_5.bungee.api.chat.TextComponent;
+import org.bukkit.ChatColor;
+import org.bukkit.Material;
+import org.bukkit.NamespacedKey;
+import org.bukkit.Sound;
+import org.bukkit.block.CreatureSpawner;
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
+import org.bukkit.persistence.PersistentDataType;
+
+import java.text.NumberFormat;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Locale;
+
+public class SpawnerUtils {
+
+    public static int getPrice(ISpawners plugin, CreatureSpawner spawner) {
+        ArrayList<Material> drops = plugin.getDrops(spawner);
+
+        if (drops == null || drops.isEmpty()) {
+            return 0;
+        } else {
+            int total = 0;
+            for (Material drop : drops) {
+                if (plugin.getConfig().getConfigurationSection("prices").contains(drop.name())) {
+                    total += plugin.getConfig().getConfigurationSection("prices").getInt(drop.name());
+                }
+            }
+            return total;
+        }
+    }
+
+    public static double getFillPercent(ISpawners plugin, CreatureSpawner spawner) {
+        double maxDrops = getMaxDrops(plugin, spawner);
+        double currentDrops = 0;
+        if (plugin.getDrops(spawner) != null) {
+            currentDrops = plugin.getDrops(spawner).size();
+        }
+        double percentage = (currentDrops / maxDrops) * 100;
+        percentage = ((double)((int)(percentage*100.0)))/100.0;
+        return percentage;
+    }
+
+    public static int getMaxDrops(ISpawners plugin, CreatureSpawner spawner) {
+        int maxDrops = plugin.getConfig().getInt("max_items");
+        int stackSize = plugin.getStackSize(spawner);
+        double multiplier = plugin.getConfig().getDouble("max_items_multiplier");
+        double finalMaxDrops = 0;
+        for (int i = 0; i < stackSize; i++) {
+            finalMaxDrops += maxDrops * multiplier;
+        }
+        return (int) finalMaxDrops;
+    }
+
+    public static String getType(ISpawners plugin, ItemStack im) {
+        return im.getItemMeta().getPersistentDataContainer().get(new NamespacedKey(plugin, "spawnerType"), PersistentDataType.STRING);
+    }
+
+    public static String formatCurrency(double money) {
+        NumberFormat fmt = NumberFormat.getCompactNumberInstance(Locale.US, NumberFormat.Style.SHORT);
+
+        return fmt.format(money);
+    }
+
+    public static void handleXP(Player player, CreatureSpawner spawner, ItemStack item, ISpawners plugin) {
+        if (!plugin.getSpawners().contains(spawner)) {
+            player.closeInventory();
+            return;
+        }
+        int xp = plugin.getXP(spawner);
+        plugin.setXP(spawner, 0);
+        player.giveExp(xp / 20);
+        ItemMeta xpMeta = item.getItemMeta();
+        xpMeta.setDisplayName(net.md_5.bungee.api.ChatColor.of("#36ffa4") + "" + ChatColor.BOLD + "COLLECT XP");
+        xpMeta.setLore(Arrays.asList(net.md_5.bungee.api.ChatColor.of("#36ffa4") + "0" + ChatColor.WHITE + " XP Points"));
+        item.setItemMeta(xpMeta);
+        if (xp > 0) {
+            player.playSound(player, Sound.ENTITY_PLAYER_LEVELUP, 1, 1);
+        }
+    }
+
+    public static void handleSell(Player player, CreatureSpawner spawner, Boolean sound, ISpawners plugin) {
+        if (!plugin.getSpawners().contains(spawner)) {
+            player.closeInventory();
+            return;
+        }
+        int price = SpawnerUtils.getPrice(plugin, spawner);
+        if (price > 0) {
+            plugin.clearDrops(plugin, spawner);
+            ISpawners.getEcon().depositPlayer(player, price);
+            player.spigot().sendMessage(ChatMessageType.ACTION_BAR, new TextComponent(ChatColor.GREEN + "+$" + SpawnerUtils.formatCurrency(price)));
+            if (sound) {
+                player.playSound(player, Sound.ENTITY_PLAYER_LEVELUP, 1, 1);
+            }
+        }
+    }
+}
