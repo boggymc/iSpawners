453ba5ce1b2ab7d0ee630dd39078e9f77b1ead52
QarthO <qartho@gmail.com>
2023-11-29 17:03:28 -0500

    refactored SpawnerBreakListener

--- src/main/java/com/boggy/ispawners/spawner/listener/SpawnerBreakListener.java
+++ src/main/java/com/boggy/ispawners/spawner/listener/SpawnerBreakListener.java
@@ -3,11 +3,13 @@ package com.boggy.ispawners.spawner.listener;
 import com.boggy.ispawners.ISpawners;
 import net.md_5.bungee.api.ChatMessageType;
 import net.md_5.bungee.api.chat.TextComponent;
+import org.apache.commons.lang.WordUtils;
 import org.bukkit.ChatColor;
 import org.bukkit.Material;
 import org.bukkit.NamespacedKey;
 import org.bukkit.block.CreatureSpawner;
 import org.bukkit.enchantments.Enchantment;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockBreakEvent;
@@ -22,68 +24,50 @@ public class SpawnerBreakListener implements Listener {
     }
 
 
+    public boolean canBreakSpawner(ItemStack item){
+//        If the item isn't a pickaxe
+        if(!item.getType().toString().contains("PICKAXE")) return false;
+//        If the item has silk touch
+        return item.hasItemMeta() && item.getItemMeta().hasEnchant(Enchantment.SILK_TOUCH);
+    }
+
     @EventHandler
     public void onSpawnerBreak(BlockBreakEvent e) {
 
+        if(!(e.getBlock().getState() instanceof CreatureSpawner spawner)) return;
 
+        Player player = e.getPlayer();
 
-        CreatureSpawner spawner;
-
-        if (e.getBlock().getState() instanceof CreatureSpawner) {
-            spawner = (CreatureSpawner) e.getBlock().getState();
-        } else {
+        if(!canBreakSpawner(player.getInventory().getItemInMainHand())){
+            e.setCancelled(true);
+            player.spigot().sendMessage(ChatMessageType.ACTION_BAR, new TextComponent(ChatColor.RED + "You need silk touch to pick up spawners!"));
             return;
         }
 
+        String spawnedType = spawner.getSpawnedType().toString();
+        if(spawnedType == null) return;
 
-        if (e.getPlayer().getInventory().getItemInMainHand().hasItemMeta() && e.getPlayer().getInventory().getItemInMainHand().getItemMeta().getEnchants().containsKey(Enchantment.SILK_TOUCH)) {
-            if (!e.getPlayer().getInventory().getItemInMainHand().getType().toString().contains("PICKAXE")) {
-                e.setCancelled(true);
-                e.getPlayer().spigot().sendMessage(ChatMessageType.ACTION_BAR, new TextComponent(ChatColor.RED + "You need silk touch to pick up spawners!"));
-                return;
-            }
+        spawnedType = WordUtils.capitalizeFully(spawnedType);
+        ItemStack spawnerDrop = new ItemStack(Material.SPAWNER);
+        ItemMeta spawnerMeta = spawnerDrop.getItemMeta();
 
-            String spawnedType = spawner.getSpawnedType().toString().toLowerCase();
-            if (spawnedType == null) {
-                return;
-            }
-            spawnedType = spawnedType.substring(0, 1).toUpperCase() + spawnedType.substring(1);
+        spawnerMeta.setDisplayName(ChatColor.RESET + "" + ChatColor.YELLOW + "" + ChatColor.BOLD + spawnedType + " spawner");
+        spawnerMeta.getPersistentDataContainer().set(new NamespacedKey(plugin, "spawnerType"), PersistentDataType.STRING, spawnedType);
+        spawnerDrop.setItemMeta(spawnerMeta);
 
-            ItemStack spawnerDrop = new ItemStack(Material.SPAWNER);
-            ItemMeta spawnerMeta = spawnerDrop.getItemMeta();
+        int stackSize = plugin.getStackSize(spawner);
+        int toRemove = player.isSneaking() ? 64 : 1;
 
-            spawnerMeta.setDisplayName(ChatColor.RESET + "" + ChatColor.YELLOW + "" + ChatColor.BOLD + spawnedType + " spawner");
-            spawnerMeta.getPersistentDataContainer().set(new NamespacedKey(plugin, "spawnerType"), PersistentDataType.STRING, spawnedType);
-            spawnerDrop.setItemMeta(spawnerMeta);
-
-            int stackSize = plugin.getStackSize(spawner);
-
-            if (e.getPlayer().isSneaking()) {
-                if ((stackSize - 64) <= 0) {
-                    plugin.removeSpawner(spawner);
-                    e.getBlock().setType(Material.AIR);
-                    spawnerDrop.setAmount(stackSize);
-                    e.getBlock().getWorld().dropItemNaturally(e.getBlock().getLocation(), spawnerDrop);
-                } else {
-                    plugin.updateStackSize(spawner, -64);
-                    spawnerDrop.setAmount(64);
-                    e.getBlock().getWorld().dropItemNaturally(e.getBlock().getLocation(), spawnerDrop);
-                }
-                e.setCancelled(true);
-            } else {
-                if ((stackSize - 1) <= 0) {
-                    plugin.removeSpawner(spawner);
-                    e.getBlock().setType(Material.AIR);
-                    e.getBlock().getWorld().dropItemNaturally(e.getBlock().getLocation(), spawnerDrop);
-                } else {
-                    plugin.updateStackSize(spawner, -1);
-                    e.getBlock().getWorld().dropItemNaturally(e.getBlock().getLocation(), spawnerDrop);
-                }
-                e.setCancelled(true);
-            }
+        if(stackSize <= 0){
+            plugin.removeSpawner(spawner);
+            e.getBlock().setType(Material.AIR);
+            e.getBlock().getWorld().dropItemNaturally(e.getBlock().getLocation(), spawnerDrop);
         } else {
-            e.setCancelled(true);
-            e.getPlayer().spigot().sendMessage(ChatMessageType.ACTION_BAR, new TextComponent(ChatColor.RED + "You need silk touch to pick up spawners!"));
+            plugin.updateStackSize(spawner, -toRemove);
+            spawnerDrop.setAmount(toRemove);
+            e.getBlock().getWorld().dropItemNaturally(e.getBlock().getLocation(), spawnerDrop);
         }
+
+        e.setCancelled(true);
     }
 }
