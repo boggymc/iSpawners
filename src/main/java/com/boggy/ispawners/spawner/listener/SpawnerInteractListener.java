e00360d5b66f4695d704f318dd91fcb3a519c147
QarthO <qartho@gmail.com>
2023-11-29 19:03:05 -0500

    Revert "fixed if stacksize is less than 0 check"
    This reverts commit 091ddbf6b3fe1d1c092d119edd72bd62e9182bfd.

--- src/main/java/com/boggy/ispawners/spawner/listener/SpawnerInteractListener.java
+++ src/main/java/com/boggy/ispawners/spawner/listener/SpawnerInteractListener.java
@@ -6,7 +6,6 @@ import com.boggy.ispawners.pdc.SpawnerUtils;
 import net.md_5.bungee.api.ChatMessageType;
 import net.md_5.bungee.api.chat.TextComponent;
 import org.bukkit.ChatColor;
-import org.bukkit.FluidCollisionMode;
 import org.bukkit.Material;
 import org.bukkit.NamespacedKey;
 import org.bukkit.block.Block;
@@ -18,79 +17,88 @@ import org.bukkit.event.Listener;
 import org.bukkit.event.block.Action;
 import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.inventory.EquipmentSlot;
-import org.bukkit.inventory.ItemStack;
 import org.bukkit.persistence.PersistentDataType;
 
 import java.util.UUID;
 
 public class SpawnerInteractListener implements Listener {
-    private final ISpawners plugin;
+    private ISpawners plugin;
     public SpawnerInteractListener(ISpawners plugin) {
         this.plugin = plugin;
     }
 
-    public boolean tryingToStack(ItemStack heldItem, CreatureSpawner spawner){
-//        Checks if they're holding a spawner
-        if(!heldItem.getType().equals(Material.SPAWNER)) return false;
-//        Gets the SpawnerType of the held spawner
-        String heldSpawnerType = SpawnerUtils.getType(plugin, heldItem);
-        if(heldSpawnerType == null) return false;
-//        Returns whether they're the same SpawnerType
-        return heldSpawnerType.equalsIgnoreCase((spawner.getSpawnedType().toString()));
-    }
-
     @EventHandler
     public void onInteract(PlayerInteractEvent e) {
-        if (e.getHand() == null) {
-            return;
-        }
-        if (e.getHand().equals(EquipmentSlot.OFF_HAND)) {
-            return;
-        }
+        if (e.getHand() == null) { return; }
+        if (e.getHand().equals(EquipmentSlot.OFF_HAND)) { return; }
         Player player = e.getPlayer();
 
-        if (!e.getAction().equals(Action.RIGHT_CLICK_BLOCK)) return;
+        if (e.getAction().equals(Action.RIGHT_CLICK_BLOCK)) {
+            if (e.getClickedBlock().getType().equals(Material.SPAWNER)) {
+                CreatureSpawner spawner = (CreatureSpawner) e.getClickedBlock().getState();
 
-        Block targetedBlock = e.getPlayer().getTargetBlockExact(5, FluidCollisionMode.NEVER);
-        if (targetedBlock == null) return;
-        if (!(targetedBlock.getState() instanceof CreatureSpawner spawner)) return;
-        if (spawner.getSpawnedType().equals(EntityType.DROPPED_ITEM)) return;
+                Block block = e.getPlayer().getTargetBlockExact(5);
+                if (block == null) {
+                    return;
+                }
+                if (block.getState() instanceof CreatureSpawner) {
+                    if (spawner.getSpawnedType() == null || spawner.getSpawnedType().equals(EntityType.DROPPED_ITEM)) {
+                        return;
+                    }
+                    if (player.getInventory().getItemInMainHand().hasItemMeta()) {
+                        if (SpawnerUtils.getType(plugin, player.getInventory().getItemInMainHand()) != null) {
+                            String spawnerType = SpawnerUtils.getType(plugin, player.getInventory().getItemInMainHand()).toUpperCase();
+                            System.out.println(spawner.getSpawnedType());
+                            if (spawner.getSpawnedType().toString().equals(spawnerType)) {
+                                e.setCancelled(true);
+                                int maxStackSize = plugin.getConfig().getInt("max_stacked_spawners");
+                                if (player.isSneaking()) {
+                                    int increase = player.getInventory().getItemInMainHand().getAmount();
+                                    if (plugin.getStackSize(spawner) + increase >= maxStackSize) {
+                                        if (plugin.getStackSize(spawner) >= maxStackSize) {
+                                            player.spigot().sendMessage(ChatMessageType.ACTION_BAR, new TextComponent(ChatColor.RED + "You have reached the maximum number of stacked spawners!"));
+                                            return;
+                                        }
+                                        increase = maxStackSize - plugin.getStackSize(spawner);
+                                        System.out.println(increase);
+                                        player.getInventory().getItemInMainHand().setAmount(player.getInventory().getItemInMainHand().getAmount() - increase);
+                                        plugin.updateStackSize(spawner, increase);
+                                        player.spigot().sendMessage(ChatMessageType.ACTION_BAR, new TextComponent(ChatColor.RED + "You have reached the maximum number of stacked spawners!"));
+                                        return;
+                                    }
+                                    player.getInventory().getItemInMainHand().setAmount(0);
+                                    plugin.updateStackSize(spawner, increase);
+                                    return;
+                                } else {
+                                    if (plugin.getStackSize(spawner) >= maxStackSize) {
+                                        player.spigot().sendMessage(ChatMessageType.ACTION_BAR, new TextComponent(ChatColor.RED + "You have reached the maximum number of stacked spawners!"));
+                                        return;
+                                    }
+                                    player.getInventory().getItemInMainHand().setAmount(player.getInventory().getItemInMainHand().getAmount() - 1);
+                                    plugin.updateStackSize(spawner, 1);
+                                    return;
+                                }
+                            } else {
+                                e.setCancelled(true);
+                                player.spigot().sendMessage(ChatMessageType.ACTION_BAR, new TextComponent(ChatColor.RED + "You cannot stack different spawner types!"));
+                                return;
+                            }
+                        }
+                    }
+                }
 
+                if (!spawner.getPersistentDataContainer().has(new NamespacedKey(plugin, "spawnerID"), PersistentDataType.STRING)) {
+                    spawner.getPersistentDataContainer().set(new NamespacedKey(plugin, "spawnerID"), PersistentDataType.STRING, String.valueOf(UUID.randomUUID()));
+                    spawner.update();
+                }
 
-        ItemStack heldItem = player.getInventory().getItemInMainHand();
-        if (tryingToStack(heldItem, spawner)){
-            plugin.getLogger().info("is trying to stack");
-            e.setCancelled(true);
-            int maxStackSize = plugin.getConfig().getInt("max_stacked_spawners");
-            int currentStackSize = plugin.getStackSize(spawner);
-            int addStackSize = player.isSneaking() ? heldItem.getAmount() : 1;
+                if (!plugin.getSpawners().contains(spawner)) {
+                    plugin.createSpawner(spawner);
+                }
+                plugin.getSpawnerUITracker().put(e.getPlayer(), spawner);
 
-            if(currentStackSize >= maxStackSize){
-                player.spigot().sendMessage(ChatMessageType.ACTION_BAR, new TextComponent(ChatColor.RED + "You have reached the maximum number of stacked spawners!"));
-                return;
+                new SpawnerUI(e.getPlayer(), spawner, plugin).open(player);
             }
-            if(currentStackSize+addStackSize > maxStackSize){
-                addStackSize = maxStackSize - currentStackSize;
-                player.spigot().sendMessage(ChatMessageType.ACTION_BAR, new TextComponent(ChatColor.RED + "You have reached the maximum number of stacked spawners!"));
-            }
-            heldItem.setAmount(heldItem.getAmount() - addStackSize);
-            plugin.updateStackSize(spawner, addStackSize);
-
-            return;
         }
-//
-        if (!spawner.getPersistentDataContainer().has(new NamespacedKey(plugin, "spawnerID"), PersistentDataType.STRING)) {
-            spawner.getPersistentDataContainer().set(new NamespacedKey(plugin, "spawnerID"), PersistentDataType.STRING, String.valueOf(UUID.randomUUID()));
-            spawner.update();
-        }
-
-        if (!plugin.getSpawners().contains(spawner)) {
-            plugin.createSpawner(spawner);
-        }
-        plugin.getSpawnerUITracker().put(e.getPlayer(), spawner);
-
-
-        new SpawnerUI(e.getPlayer(), spawner, plugin).open(player);
-
     }
 }
